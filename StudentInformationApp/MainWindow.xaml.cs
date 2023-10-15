2ad684ef50000717551085ae689d8743a7b44c09
Bogdankmennyy <bogdankmennyj@gmail.com>
2023-10-15 09:27:05 +0300

    finished work

--- StudentInformationApp/MainWindow.xaml.cs
+++ StudentInformationApp/MainWindow.xaml.cs
@@ -1,28 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿//Розробіть додаток, який вміє запускати дочірній процес і чекати
+//на його завершення. Коли дочірній процес завершено,
+//батьківський додаток має відобразити код завершення.
 
-namespace StudentInformationApp
+
+using System;
+using System.Diagnostics;
+using System.Windows;
+
+namespace ProcessLauncherApp
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void LaunchProcessButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Шлях до виконуваного файлу дочірнього процесу
+            string processPath = "ШЛЯХ_ДО_ФАЙЛУ_ДОЧІРНОГО_ПРОЦЕСУ.exe";
+
+            // Створення процесу та його налаштування
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                FileName = processPath,
+                UseShellExecute = false, // Використовувати прямий запуск (не через оболонку)
+                RedirectStandardOutput = true, // Перенаправити стандартний вивід
+                CreateNoWindow = true, // Не створювати вікно дочірнього процесу
+            };
+
+            Process process = new Process { StartInfo = startInfo };
+
+            process.EnableRaisingEvents = true; // Дозволити обробку подій завершення
+
+            // Обробник події для завершення дочірнього процесу
+            process.Exited += (s, args) =>
+            {
+                int exitCode = process.ExitCode;
+                Dispatcher.Invoke(() =>
+                {
+                    ResultTextBlock.Text = $"Код завершення: {exitCode}";
+                });
+                process.Dispose();
+            };
+
+            // Запускаємо дочірній процес
+            process.Start();
+        }
     }
 }
+
