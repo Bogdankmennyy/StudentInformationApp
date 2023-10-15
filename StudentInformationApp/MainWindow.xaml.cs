042ce304987fa50c00496ddc5f88cbaf96e87cd6
Bogdankmennyy <bogdankmennyj@gmail.com>
2023-10-15 09:08:32 +0300

    finished work

--- StudentInformationApp/MainWindow.xaml.cs
+++ StudentInformationApp/MainWindow.xaml.cs
@@ -1,28 +1,151 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿//Реалізуйте клас «Студент». Необхідно зберігати в
+//змінних-членах класу: ПІБ, дату народження, контактний
+//телефон, місто, країну, назву навчального закладу, місто
+//та країну (де знаходиться навчальний заклад), номер
+//групи.Реалізуйте функції-члени класу для введення даних, виведення даних, реалізуйте аксесор для доступу до
+//окремих змінних-членів.
 
-namespace StudentInformationApp
+
+using System;
+using System.ComponentModel;
+using System.Windows;
+
+namespace StudentInfoWPF
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public class Student : INotifyPropertyChanged
     {
-        public MainWindow()
+        private string fullName;
+        private DateTime birthDate;
+        private string phoneNumber;
+        private string city;
+        private string country;
+        private string schoolName;
+        private string schoolCity;
+        private string schoolCountry;
+        private string groupNumber;
+
+        public string FullName
         {
-            InitializeComponent();
+            get { return fullName; }
+            set
+            {
+                if (fullName != value)
+                {
+                    fullName = value;
+                    OnPropertyChanged("FullName");
+                }
+            }
+        }
+
+        public DateTime BirthDate
+        {
+            get { return birthDate; }
+            set
+            {
+                if (birthDate != value)
+                {
+                    birthDate = value;
+                    OnPropertyChanged("BirthDate");
+                }
+            }
+        }
+
+        public string PhoneNumber
+        {
+            get { return phoneNumber; }
+            set
+            {
+                if (phoneNumber != value)
+                {
+                    phoneNumber = value;
+                    OnPropertyChanged("PhoneNumber");
+                }
+            }
+        }
+
+        public string City
+        {
+            get { return city; }
+            set
+            {
+                if (city != value)
+                {
+                    city = value;
+                    OnPropertyChanged("City");
+                }
+            }
+        }
+
+        public string Country
+        {
+            get { return country; }
+            set
+            {
+                if (country != value)
+                {
+                    country = value;
+                    OnPropertyChanged("Country");
+                }
+            }
+        }
+
+        public string SchoolName
+        {
+            get { return schoolName; }
+            set
+            {
+                if (schoolName != value)
+                {
+                    schoolName = value;
+                    OnPropertyChanged("SchoolName");
+                }
+            }
+        }
+
+        public string SchoolCity
+        {
+            get { return schoolCity; }
+            set
+            {
+                if (schoolCity != value)
+                {
+                    schoolCity = value;
+                    OnPropertyChanged("SchoolCity");
+                }
+            }
+        }
+
+        public string SchoolCountry
+        {
+            get { return schoolCountry; }
+            set
+            {
+                if (schoolCountry != value)
+                {
+                    schoolCountry = value;
+                    OnPropertyChanged("SchoolCountry");
+                }
+            }
+        }
+
+        public string GroupNumber
+        {
+            get { return groupNumber; }
+            set
+            {
+                if (groupNumber != value)
+                {
+                    groupNumber = value;
+                    OnPropertyChanged("GroupNumber");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
