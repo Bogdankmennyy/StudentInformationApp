ac30652e97ce1749c7e2ef33b093430a76add475
Bogdankmennyy <bogdankmennyj@gmail.com>
2023-10-15 09:38:02 +0300

    finished work

--- StudentInformationApp/MainWindow.xaml.cs
+++ StudentInformationApp/MainWindow.xaml.cs
@@ -1,28 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿//Розробіть додаток, який відображає список процесів. Користувач
+//може вказати інтервал часу для оновлення списку. Обов'язково
+//створіть віконний інтерфейс додатка.
 
-namespace StudentInformationApp
+
+using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace ProcessListApp
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private DispatcherTimer timer;
+        private Process[] processes;
+
         public MainWindow()
         {
             InitializeComponent();
+            InitializeTimer();
+        }
+
+        private void InitializeTimer()
+        {
+            timer = new DispatcherTimer();
+            timer.Tick += Timer_Tick;
+            timer.Interval = TimeSpan.FromSeconds(1); // Інтервал оновлення у секундах
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            UpdateProcessList();
+        }
+
+        private void StartUpdateButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(IntervalTextBox.Text, out int intervalSeconds))
+            {
+                timer.Interval = TimeSpan.FromSeconds(intervalSeconds);
+                timer.Start();
+            }
+            else
+            {
+                MessageBox.Show("Введіть коректний інтервал оновлення (у секундах).");
+            }
+        }
+
+        private void UpdateProcessList()
+        {
+            processes = Process.GetProcesses();
+            ProcessDataGrid.ItemsSource = processes;
         }
     }
 }
+
